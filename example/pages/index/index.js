a5ce84c9809432ec77027aadb61f765915ac7a24
zhanziyang <zhanziyang126@hotmail.com>
2017-07-14 15:23:34 +0800

    change sample text

--- example/pages/index/index.js
+++ example/pages/index/index.js
@@ -3,8 +3,8 @@ var sample = require('../../sample')
 
 Page({
   onLoad() {
-    var wxmlify = new Wxmlify(sample.example2, this, {
-      preserveStyles: 'all',
+    var wxmlify = new Wxmlify(sample.example1, this, {
+      preserveStyles: ['fontSize', 'fontWeight', 'fontStyle', 'color', 'textDecoration', 'textAlign', 'backgroundColor', 'background'],
       dataKey: 'nodes',
       disableImagePreivew: false,
       onImageTap: function (evt) {
